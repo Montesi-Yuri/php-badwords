fdce1175825e103bbe952fbcbacef1272e0cca4c
Montesi-Yuri <yuri13312@gmail.com>
2023-08-21 15:59:42 +0200

    ADD - php var and form

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php   
 
+    $paragraph = $_GET['paragraph'];
+    $censorWord = $_GET['censorship'];
 
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +15,21 @@
     </head>
     <body>
         <main>
-            
+
+            <form method="get">
+                <textarea name="paragraph" id="" rows="4" cols="50"></textarea>
+                <br>
+                <input type="text" name="censorship" id="">
+                <button>Applica censura</button>
+            </form>
+
+            <div>
+                <?php 
+                    echo $paragraph;
+                    echo '<br>';
+                    echo $censorWord;
+                ?>
+            </div>
             
         </main>
         
